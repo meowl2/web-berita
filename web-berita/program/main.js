99f55daabecb0108c3c65eb6bd61131bcaf57b88
xern <midjuambo@gmail.com>
2026-05-02 13:05:53 +0700

    a

--- web-berita/program/main.js
+++ web-berita/program/main.js
@@ -22,9 +22,3 @@ const sr = ScrollReveal({
   delay: 100,
   reset: true,
 });
-
-sr.reveal(".container", { origin: "bottom" });
-sr.reveal("", {});
-sr.reveal(".berita_1 div", { interval: 200 });
-sr.reveal(".berita_2 div, .trending div", { interval: 100 });
-
